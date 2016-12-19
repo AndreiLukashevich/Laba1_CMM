d56e4da9d0ff5b2757553011699bdff248d1390d
AndreiLukashevich <rid7003482@gmail.com>
2016-12-19 23:53:12 +0300

    Refactoring code (add comments)

--- app.py
+++ app.py
@@ -20,18 +20,18 @@ for i in range(int(len(corpus['a'])*0.8)):
     concat_a += corpus['a'][i]
 
 concat_a = np.asarray(concat_a)
-print (concat_a)
-print (lengths_a)
-print (len(corpus['a']))
-print (int(len(corpus['a'])*0.8))
-print (len(lengths_a))
-print (len(concat_a))
+print('----------DATA PHONEME "a"----------')
+# print (concat_a)
+# print (lengths_a)
+print ("lengths all set ", len(corpus['a']))
+print ("lengths leraning set ", int(len(corpus['a'])*0.8))
+print ("lengths test set ", (len(corpus['a']) - int(len(corpus['a'])*0.8)))
+print ("list with lengths appropriate initial sequence ", len(lengths_a))
+print ("long sequence (concatenate) ", len(concat_a))
 
 # create model for phoneme 'a'
 model_a.fit(concat_a, lengths_a)
 #print (model_a)
-
-print('-------------------------------------------------------')
 
 
 #leraning phoneme 'n', lengths leraning set l_n = len(corpus['n'])*0.8 = 77
@@ -42,20 +42,21 @@ for i in range(int(len(corpus['n'])*0.8)):
     concat_n += corpus['n'][i]
 
 concat_n = np.asarray(concat_n)
-print (concat_n)
-print (lengths_n)
-print (len(corpus['n']))
-print (int(len(corpus['n'])*0.8))
-print (len(lengths_n))
-print (len(concat_n))
+print('----------DATA PHONEME "n"----------')
+# print (concat_n)
+# print (lengths_n)
+print ("lengths all set ", len(corpus['n']))
+print ("lengths leraning set ", int(len(corpus['n'])*0.8))
+print ("lengths test set ", (len(corpus['n']) - int(len(corpus['n'])*0.8)))
+print ("list with lengths appropriate initial sequence ", len(lengths_n))
+print ("long sequence (concatenate) ", len(concat_n))
 
 model_n.fit(concat_n, lengths_n)
 #print (model_n)
 
-print('------------------------------------------------------')
 
-
-# recognition phoneme 'a', using model_a, lengths set
+# recognition phoneme 'a', using model_a, lengths set = all lengths set for phoneme 'n', t.k.
+#80%*lengths set for phoneme 'a' > all lengths set for phoneme 'n'
 a_true = 0
 a_false = 0
 for i in range(int(len(corpus['n']))):
@@ -66,8 +67,15 @@ for i in range(int(len(corpus['n']))):
     else:
         a_false += 1
 
+print('----------RECOGNITION PHONEME "a"----------')
 print("a_true", a_true)
 print("a_false", a_false)
+
+# recognition accuracy for phoneme 'a' lengths tes set N = 224-179 = 45, quantity true recognition n = 47
+N_a = 45.0
+n_a = 47.0
+acc_a = n_a/N_a
+print ("recognition accuracy for phoneme 'a', acc_a= ",acc_a)
 
 # recognition phoneme 'n', using model_n, lengths set = leraning set l_n = len(corpus['n'])*0.8 = 77
 n_true = 0
@@ -80,21 +88,13 @@ for i in range(int(len(corpus['n'])*0.8)):
     else:
         n_false += 1
 
+print('----------RECOGNITION PHONEME "n"----------')
 print("n_true", n_true)
 print("n_false", n_false)
-
-print('------------------------------------------------------')
-
-
-# recognition accuracy for phoneme 'a' lengths tes set N = 224-179 = 45, quantity true recognition n = 47
-N_a = 45.0
-n_a = 47.0
-acc_a = n_a/N_a
-print ("recognition accuracy for phoneme 'a', acc_a=",acc_a)
 
 
 # recognition accuracy for phoneme 'n' lengths tes set N = 97-77 = 20, quantity true recognition n = 54
 N_n = 20.0
 n_n = 54.0
 acc_n = n_n/N_n
-print ("recognition accuracy for phoneme 'n', acc_n=",acc_n)
+print ("recognition accuracy for phoneme 'n', acc_n= ",acc_n)
